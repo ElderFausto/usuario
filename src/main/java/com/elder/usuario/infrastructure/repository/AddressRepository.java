7094744dd9051001a78c26106af8eb81182781a8
ElderFavsto <elderfavsto@gmail.com>
2025-08-26 14:48:11 -0300

    adiciona classes de security

---
+++ src/main/java/com/elder/usuario/infrastructure/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.elder.usuario.infrastructure.repository;
+
+import com.jimpo.apirestjava.infrastructure.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
